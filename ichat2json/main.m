e1af87b0235030152509ffbef247ef19c0174580
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-12-09 18:42:16 -0800

    Initial Commit

---
+++ ichat2json/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ichat2json
+//
+//  Created by Keyvan Fatehi on 12/9/16.
+//  Copyright © 2016 Keyvan Fatehi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
